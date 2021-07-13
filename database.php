b3dad84fccc1118cbd61fd5397f8c500b52c416b
progmamun <mamun333089@gmail.com>
2021-07-13 21:32:48 +0600

    start Projects

---
+++ database.php
@@ -0,0 +1,34 @@
+<?php
+
+class Database
+{
+    public function __construct()
+    {
+
+    }
+
+    // Function to insert into the database
+    public function insert()
+    {
+
+    }
+
+    // Function to delete table or row(s) from database
+    public function delete()
+    {
+
+    }
+
+    // Function to SELECT from the database
+    public function select()
+    {
+
+    }
+
+    // close connection
+    public function __destruct()
+    {
+
+    }
+
+} //class close
