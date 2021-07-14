b1cc63b31e1b2962b01baf92d56e4308de501711
progmamun <mamun333089@gmail.com>
2021-07-14 22:55:59 +0600

    construct update

--- database.php
+++ database.php
@@ -15,7 +15,7 @@ class Database
     {
         if (!$this->conn) {
             $this->mysqli = new mysqli($this->db_host, $this->db_user, $this->db_pass, $this->db_name);
-
+            $this->conn = true;
             if ($this->mysqli->connect_error) {
                 array_push($this->result, $this->mysqli->connect_error);
                 return false;
