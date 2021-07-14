f3e7d259179f08feb27a315c3a3cd526951a1add
progmamun <mamun333089@gmail.com>
2021-07-14 22:45:32 +0600

    construct function run

--- database.php
+++ database.php
@@ -2,9 +2,27 @@
 
 class Database
 {
+    private $db_host = "localhost";
+    private $db_user = "root";
+    private $db_pass = "";
+    private $db_name = "testing";
+
+    private $mysqli = "";
+    private $result = array();
+    private $conn = false;
+
     public function __construct()
     {
+        if (!$this->conn) {
+            $this->mysqli = new mysqli($this->db_host, $this->db_user, $this->db_pass, $this->db_name);
 
+            if ($this->mysqli->connect_error) {
+                array_push($this->result, $this->mysqli->connect_error);
+                return false;
+            }
+        } else {
+            return true;
+        }
     }
 
     // Function to insert into the database
